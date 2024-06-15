8b117ee0a301d19804095abb8a5714927aa742f5
sirius1y <sirius1y@outlook.com>
2024-06-15 14:48:52 +0800

    feat: 新增logger配置

--- frontend/lib/conf/config.dart
+++ frontend/lib/conf/config.dart
@@ -1,2 +1,13 @@
 // config.dart
+import 'package:logger/logger.dart';
+
+// api base url
 const String apiDomain = '127.0.0.1';
+
+// logger
+Logger getLogger() {
+  return Logger(
+    printer: PrettyPrinter(),
+    filter: ProductionFilter(),
+  );
+}
