365491ef33bbe431a36fab127ee25fdc46a7b1c3
sirius1y <sirius1y@outlook.com>
2024-06-15 16:15:10 +0800

    refactor:新增三个待写的页面，poll_content.dart被poll_detail_page.dart代替

---
+++ frontend/lib/screens/search_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  final TabController tabController;
+
+  const SearchPage({Key? key, required this.tabController}) : super(key: key);
+
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = widget.tabController;
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search',
+            border: InputBorder.none,
+          ),
+          onSubmitted: (value) {
+            // 搜索
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // 清空搜索框
+              _searchController.clear();
+            },
+            icon: const Icon(Icons.clear),
+          ),
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          // 搜索结果
+        ],
+      ),
+    );
+  }
+}
