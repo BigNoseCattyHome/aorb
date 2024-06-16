9114269111b449f72ca9b54056cc159fcff0a325
Henry <sirius1y@outlook.com>
2024-06-17 02:03:59 +0800

    feat:用户登录还差和数据库查询部分

--- backend/go-services/auth/main.go
+++ backend/go-services/auth/main.go
@@ -4,16 +4,12 @@ package main
 import (
 	"os"
 
+	"github.com/BigNoseCattyHome/aorb/backend/go-services/api-gateway/conf"
 	"github.com/BigNoseCattyHome/aorb/backend/go-services/auth/routes"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	r := gin.Default()   // 这里是gin框架的方法，用来创建一个gin的实例
-	routes.AuthRoutes(r) // 这里调用我们自己的routes.go中的AuthRoutes方法，传入r参数
-	r.Run(":8080")       // 让gin框架监听8080端口
-}
 func init() {
 	// 设置日志级别为 Debug
 	log.SetLevel(log.DebugLevel)
@@ -23,4 +19,15 @@ func init() {
 
 	// 设置日志输出到标准输出
 	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	// 初始化config配置文件
+	if err := conf.LoadConfig(); err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	r := gin.Default()   // 这里是gin框架的方法，用来创建一个gin的实例
+	routes.AuthRoutes(r) // 这里调用我们自己的routes.go中的AuthRoutes方法，传入r参数
+	r.Run(":8080")       // 让gin框架监听8080端口
 }
--- backend/go-services/auth/services/auth_service.go
+++ backend/go-services/auth/services/auth_service.go
@@ -1,74 +1,51 @@
 package services
 
 import (
-	"crypto/md5"
-	"encoding/hex"
+	"context"
 	"errors"
+	"fmt"
 	"os"
-	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/BigNoseCattyHome/aorb/backend/go-services/auth/conf"
 	"github.com/BigNoseCattyHome/aorb/backend/go-services/auth/models"
-	"github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
 )
 
-// 从环境变量中获取JWT密钥
-var jwtKey = []byte(os.Getenv("AORB_SECRET_KEY"))
+var jwtKey = []byte(os.Getenv("AORB_SECRET_KEY")) // 从环境变量中获取JWT密钥，用于生成JWT令牌
+var client *mongo.Client                          // MongoDB客户端
 
-// 声明（Claims）结构体
-type Claims struct {
-	Username string `json:"username"`
-	jwt.StandardClaims
+// init函数在main函数之前执行
+func init() {
+	// 设置客户端选项，从AppConfig中获取MongoDB的URL
+	clientOptions := options.Client().ApplyURI(conf.AppConfig.GetString("db.mongodb_url"))
+
+	// 连接到MongoDB
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 检查连接
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
 
-// 注册用户函数
+// 注册
 func RegisterUser(user *models.User) error {
-	// 使用MD5哈希用户密码
-	hasher := md5.New()
-	hasher.Write([]byte(user.Password))
-	user.Password = hex.EncodeToString(hasher.Sum(nil))
-
-	// 将用户保存到数据库（假设你有一个函数来执行此操作）
-	err := storeUser(user)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
+	// 在这里写注册用户的逻辑
 
 	return nil
-}
-
-// 认证用户函数
-func AuthenticateUser(user *models.User) (string, error) {
-	// 检查用户凭据
-	dbUser, err := getUser(user.Username)
-	if err != nil {
-		return "", err
-	}
-
-	// 使用MD5哈希输入的密码并进行比较
-	hasher := md5.New()
-	hasher.Write([]byte(user.Password))
-	if dbUser.Password != hex.EncodeToString(hasher.Sum(nil)) {
-		return "", errors.New("用户名或密码无效")
-	}
-
-	// 创建JWT令牌
-	expirationTime := time.Now().Add(1 * time.Hour)
-	claims := &Claims{
-		Username: user.Username,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
 }
 
 // 将用户保存到数据库
@@ -78,11 +55,63 @@ func storeUser(user *models.User) error {
 	return nil
 }
 
+// 验证用户（登录）
+// 返回(成功): JWT令牌, 用户信息, nil
+// 返回(失败): "", 空的SimpleUser, 错误信息
+func AuthenticateUser(user *models.RequestLogin) (string, models.SimpleUser, error) {
+	// 检查用户是否存在
+	storedUser, err := getUser(user.ID)
+	if err != nil {
+		log.Error("Failed to get user from database: ", err)
+		return "", models.SimpleUser{}, errors.New("failed to get user from database")
+	}
+	// 检查用户名对应的密码是否正确
+	if checkPassword(user.ID, user.Password) {
+		log.Error("Invalid password")
+		return "", models.SimpleUser{}, errors.New("invalid password")
+	}
+
+	// 生成JWT令牌
+	tokenString, err := generateJWTToken(storedUser)
+	if err != nil {
+		log.Error("Failed to generate JWT token: ", err)
+		return "", models.SimpleUser{}, errors.New("failed to generate JWT token")
+	}
+
+	// 全部顺利执行，返回用户的基本信息
+	simple_user := models.SimpleUser{
+		ID:        storedUser.ID,
+		Nickname:  storedUser.Nickname,
+		Avatar:    storedUser.Avatar,
+		Ipaddress: storedUser.Ipaddress,
+	}
+
+	return tokenString, simple_user, nil
+}
+
 // 从数据库获取用户
 func getUser(username string) (*models.User, error) {
-	// 实现此函数以从数据库获取用户
-	return &models.User{
-		Username: username,
-		Password: "hashed_password", // 替换为数据库中实际的哈希密码
-	}, nil
+	user := models.User{}
+
+	// 如果执行失败，user将保持零值
+	client.Database("aorb").Collection("users").FindOne(context.TODO(), bson.M{"id": username}).Decode(&user)
+
+	return &user, nil
+}
+
+// 检查密码是否匹配
+func checkPassword(id string, password string) bool {
+	hashedPassword := ""
+
+	// 根据id查询数据库中的哈希密码
+	client.Database("aorb").Collection("users").FindOne(context.TODO(), bson.M{"id": id}).Decode(&hashedPassword)
+
+	return password == hashedPassword
+}
+
+// 生成JWT令牌
+func generateJWTToken(user *models.User) (string, error) {
+	// 在这里生成JWT令牌
+
+	return "", nil
 }
