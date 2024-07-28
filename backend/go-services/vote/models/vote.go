60ae1f2393a9e2bacf8fae06d8ab8b6faaed304c
jinziguan <18917950960@163.com>
2024-07-28 17:57:33 +0800

    完成了poll service的重构，除了ListPoll以外的接口都通过了测试
    完成了poll service的重构，除了ListPoll以外的接口都通过了测试

--- backend/go-services/vote/models/vote.go
+++ backend/go-services/vote/models/vote.go
@@ -1,12 +1,12 @@
 package models
 
-import "google.golang.org/protobuf/types/known/timestamppb"
+import "time"
 
 type Vote struct {
 	// 与mongoDB交互的实体
-	VoteUuid     string                 `json:"vote_uuid" bson:"voteUuid,omitempty"`
-	VoteUserName string                 `json:"vote_username" bson:"voteUserName,omitempty"`
-	Choice       string                 `json:"choice" bson:"choice,omitempty"`
-	CreateAt     *timestamppb.Timestamp `json:"create_at" bson:"create_at,omitempty"`
-	DeleteAt     *timestamppb.Timestamp `json:"delete_at" bson:"delete_at"`
+	VoteUuid     string    `json:"vote_uuid" bson:"voteUuid,omitempty"`
+	VoteUserName string    `json:"vote_username" bson:"voteUserName,omitempty"`
+	Choice       string    `json:"choice" bson:"choice,omitempty"`
+	CreateAt     time.Time `json:"create_at" bson:"createAt,omitempty"`
+	DeleteAt     time.Time `json:"delete_at" bson:"deleteAt"`
 }
