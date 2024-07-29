18dcfe0fdc5a48196950bb089135115e35bc0daf
sirius1y <sirius1y@outlook.com>
2024-07-29 21:04:06 +0800

    feat：顶部状态栏实现透明效果；前端增加设置页面中清除本地的token（相当于清除用户偏好设置，在登录成功之后需要清除掉本地的kv存储重新登录，还在迭代过程中没加注销登录的功能）

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'routes/router.dart';
 import 'package:logger/logger.dart';
 
@@ -6,6 +7,15 @@ import 'package:aorb/conf/config.dart';
 
 void main() {
   var logger = getLogger();
+  
+  // 设置状态栏透明
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // 设置状态栏透明
+      statusBarIconBrightness: Brightness.dark, // 设置状态栏图标的亮度
+    ),
+  );
+
   runApp(Aorb(logger: logger));
 }
 
